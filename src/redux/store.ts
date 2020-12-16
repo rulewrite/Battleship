53e52daae203be63ab5a9cb9d75d3e9703a5fc5b
rulewrite <rulewrite@gmail.com>
2020-12-17 02:04:15 +0900

    feat: 개발모드 redux-devtools 크롬 익스텐션 설정

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,17 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const configureStore = (preloadedState?: any) =>
-  createStore(rootReducer, preloadedState);
+const configureStore = (preloadedState?: any) => {
+  if (process.env.NODE_ENV === 'production') {
+    return createStore(rootReducer, preloadedState);
+  }
+
+  return createStore(
+    rootReducer,
+    preloadedState,
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  );
+};
 
 export default configureStore;
