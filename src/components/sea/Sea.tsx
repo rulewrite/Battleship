b757ef807b553239ea3fecbd5330ff7dd9973f09
rulewrite <rulewrite@gmail.com>
2020-12-17 21:26:45 +0900

    chore: Sea 컴포넌트 인자 수정

--- src/components/sea/Sea.tsx
+++ src/components/sea/Sea.tsx
@@ -1,17 +1,15 @@
 import { Button, withStyles } from '@material-ui/core';
 import { blue } from '@material-ui/core/colors';
 
-const Sea = withStyles(() => {
-  return {
-    root: {
-      borderRight: '1px solid white',
-      borderRadius: 0,
-      backgroundColor: blue[300],
-      '&:hover': {
-        backgroundColor: blue[700],
-      },
+const Sea = withStyles({
+  root: {
+    borderRight: '1px solid white',
+    borderRadius: 0,
+    backgroundColor: blue[300],
+    '&:hover': {
+      backgroundColor: blue[700],
     },
-  };
+  },
 })(Button);
 
 export default Sea;
