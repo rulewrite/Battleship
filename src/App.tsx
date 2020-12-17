6a40ab6ae6523ab5b6f26b84b6cfd751d80d0a4f
rulewrite <rulewrite@gmail.com>
2020-12-17 22:23:58 +0900

    feat: 기본 레이아웃 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import {
+  Container,
+  createStyles,
+  Grid,
+  Theme,
+  WithStyles,
+  withStyles,
+} from '@material-ui/core';
 import { Cell, Sea } from '@Components';
+import { compose } from 'redux';
 
-const App = () => {
+const styles = (theme: Theme) =>
+  createStyles({
+    container: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
+  });
+
+const App = ({ classes }: WithStyles<typeof styles>) => {
   return (
-    <Container component="main">
-      <Cell>A</Cell>
-      <Sea>B</Sea>
+    <Container component="main" className={classes.container}>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Cell>A</Cell>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Sea>B</Sea>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
 
-export default App;
+export default compose(withStyles(styles))(App);
