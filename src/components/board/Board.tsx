877ae1c61bfdf9ce4e2f6b737bb3fc49bc92d362
rulewrite <rulewrite@gmail.com>
2020-12-17 23:32:13 +0900

    feat: 보드 동적 렌더링으로 변경

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -8,7 +8,7 @@ import {
   Theme,
   WithStyles,
 } from '@material-ui/core';
-import { Cell, Sea } from '@Components';
+import { Sea } from '@Components';
 import { Rows } from 'redux/reducers/board';
 
 const styles = (theme: Theme) =>
@@ -22,41 +22,24 @@ const Board = ({ classes, rows }: WithStyles<typeof styles> & BoardProps) => {
   console.log(rows);
   return (
     <Paper className={classes.paper}>
-      <Grid container>
-        <Grid item xs>
-          <Cell>B</Cell>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-        <Grid item xs>
-          <Sea>A</Sea>
-        </Grid>
-      </Grid>
+      {rows
+        .map((columns, rowKey) => {
+          console.log(columns.toJS(), rowKey);
+          return (
+            <Grid container key={rowKey}>
+              {columns
+                .map((column, columnKey) => {
+                  return (
+                    <Grid item xs key={`${rowKey}-${columnKey}`}>
+                      <Sea>{`${rowKey}-${columnKey}`}</Sea>
+                    </Grid>
+                  );
+                })
+                .toList()}
+            </Grid>
+          );
+        })
+        .toList()}
     </Paper>
   );
 };
